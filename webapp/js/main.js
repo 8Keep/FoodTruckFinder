30825dfbf7ff349ecba934e9f90f4c10579d6941
Davis Rollman <drollma@hotmail.com>
2018-11-16 16:24:27 -0500

    registration

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,5 +1,22 @@
  
 $( document ).ready(function() {
     $("#regfoot").hide();
-    $("#firstchoice").hide();
+    $("#ven-reg").hide();
+    $("#ft-reg").hide();
 });
+
+function regft() {
+    $("#typesel").hide();
+    $("#ft-reg").show();
+}
+
+function regven() {
+    $("#typesel").hide();
+    $("#ven-reg").show();
+}
+
+function back() {
+    $("#ft-reg").hide();
+    $("#ven-reg").hide();
+    $("#typesel").show();
+}
