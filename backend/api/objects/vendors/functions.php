955bf10a5e4c70384c4303d956028163c809ae43
Knights17 <42991228+Knights17@users.noreply.github.com>
2018-11-30 16:57:20 -0500

    Update functions.php

--- backend/api/objects/vendors/functions.php
+++ backend/api/objects/vendors/functions.php
@@ -25,6 +25,7 @@ class Vendor{
     public $first;
     public $last;
     public $imgURL;
+    public $description;
 
     // constructor with $db as database connection
     public function __construct($db){
@@ -54,10 +55,10 @@ class Vendor{
     }
 
   // edit vendor details
-  function edit($username, $ET_name, $address, $city, $state, $zip, $first, $last, $email, $phone){
+  function edit($username, $ET_name, $address, $city, $state, $zip, $first, $last, $email, $phone, $imgURL, $description){
 
       // query to insert vendor details
-      $query = "INSERT INTO " . $this->table_name . " (EntertainerName, address, city, state, zip, ETID, First, Last, email, phone) VALUES (?,?,?,?,?, (SELECT ETID from loginET WHERE username = ?),?,?,?,?)";
+      $query = "INSERT INTO " . $this->table_name . " (EntertainerName, address, city, state, zip, ETID, First, Last, email, phone, imgURL, Description) VALUES (?,?,?,?,?, (SELECT ETID from loginET WHERE username = ?),?,?,?,?,?,?)";
 
       // prepare query
       $stmt = $this->conn->prepare($query);
@@ -92,6 +93,12 @@ class Vendor{
       
       $phone=htmlspecialchars(strip_tags($phone));
       $phone = "{$phone}";
+    
+      $imgURL =htmlspecialchars(strip_tags($imgURL));
+      $imgURL = "{$imgURL}";
+      
+      $description=htmlspecialchars(strip_tags($description));
+      $description = "{$description}";
 
 
       // bind
@@ -105,6 +112,8 @@ class Vendor{
       $stmt->bindParam(8, $last);
       $stmt->bindParam(9, $email);
       $stmt->bindParam(10, $phone);
+      $stmt->bindParam(11, $imgURL);
+      $stmt->bindParam(12, $description);
 
 
       // execute query
