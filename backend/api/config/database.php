cdab481bdfe7323be538f1a52ddbc35063785361
Knights17 <42991228+Knights17@users.noreply.github.com>
2018-11-30 20:29:52 -0500

    Update database.php

--- backend/api/config/database.php
+++ backend/api/config/database.php
@@ -2,12 +2,22 @@
 class Database{
 
     // specify your own database credentials
+    /*
     private $host = "localhost";
     private $db_name = "largeproject";
     private $username = "root";
     private $password = "";
     public $conn;
-
+    */
+    private $host = "cop4331.cm0oj9xyv2kx.us-east-2.rds.amazonaws.com";
+    private $port = "3306"; 
+    private $db_name = "cop4331";
+    private $username = "spongebob7";
+    private $password = "spongebob7";
+    public $conn;
+    
+    
+    
     // get the database connection
     public function getConnection(){
 
