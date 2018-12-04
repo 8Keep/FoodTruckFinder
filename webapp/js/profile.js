37a45a78ce14bc854ade106a58d1cbd3e0245e87
andrew-kwong <42681525+andrew-kwong@users.noreply.github.com>
2018-12-04 16:26:47 -0500

    minor additions

--- webapp/js/profile.js
+++ webapp/js/profile.js
@@ -5,9 +5,8 @@ $( document ).ready(function() {
 	$("#state").hide(); 
 	$("#description").hide(); 
 	$("#submitbutton").hide(); 
+	$("#zipcode").hide(); 
 });
-
-
 
 function editData() {
 
@@ -31,11 +30,18 @@ function editData() {
 	$("#state").show();
 	$("#state").val($("#stateField").text());
 
+	$("#zipcodeField").hide();
+	$("#zipcode").show();
+	$("#zipcode").val($("#zipcodeField").text());
+
+
 	$("#descriptionField").hide();
 	$("#description").show();
 	$("#description").val($("#descriptionField").text());
 
 	$("#submitbutton").show(); 
+
+
 
 }
 
@@ -44,6 +50,26 @@ function submitData() {
 	$("#submitbutton").hide(); 
 	$("#editbutton").show(); 
 
+
+	var url = loginFT ? "https://peopleorderourpatties.com/backend/vendors/edit.php";
+	var submit = {  username:Cookies.get("username"),
+		              	telephone: $("#telephone").val(),
+						email: $("#email").val(),
+						city: $("#city").val(),
+						state: $("#state").val(),
+						zipcode: $("#zipcode").val(), 
+						description: $("#description").val()
+								 };
+
+								 var response;
+								 $.post(
+										 url,
+										 JSON.stringify(submit),
+										 function(result)
+										 {
+												 r = result.message;
+												 console.log(r);
+											 });
 
 }
 
