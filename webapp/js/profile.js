f4daf3a808b82e18525281e4f02f308fba62059e
Davis Rollman <drollma@hotmail.com>
2018-12-04 16:47:00 -0500

    fixing andrews horrible mistakes

--- webapp/js/profile.js
+++ webapp/js/profile.js
@@ -7,6 +7,13 @@ $( document ).ready(function() {
 	$("#submitbutton").hide(); 
 	$("#zipcode").hide(); 
     $("#cancelbutton").hide();
+    $("#editButton").hide();
+    
+    if (Cookies.get("profile") == Cookies.get("username"))
+    {
+        //this profile is logged in user. Allow editing.
+        $("#editButton").show();
+    }
 });
 
 function editData() {
@@ -52,26 +59,39 @@ function submitData() {
 	$("#submitbutton").hide(); 
 	$("#editbutton").show(); 
 
+    var url;
+    
+    if (Cookies.get("type") == "foodtruck")
+    {
+        url = "https://peopleorderourpatties.com/backend/api/users/vendors/edit.php";
+    }
+    else
+    {
+        url = "https://peopleorderourpatties.com/backend/api/users/foodtrucks/edit.php";
+    }
+    if (Cookies.get("profile") == Cookies.get("username"))
+    {
+        url = "https://peopleorderourpatties.com/backend/api/users/" + Cookies.get("type") + "s/edit.php";
+    }
+    
+    var submit = {  username : Cookies.get("username"),
+                        telephone : $("#telephone").val(),
+                        email : $("#email").val(),
+                        city : $("#city").val(),
+                        state : $("#state").val(),
+                        zipcode : $("#zipcode").val(), 
+                        description : $("#description").val()};
 
-	var url = loginFT ? "https://peopleorderourpatties.com/backend/vendors/edit.php";
-	var submit = {  username:Cookies.get("username"),
-		              	telephone: $("#telephone").val(),
-						email: $("#email").val(),
-						city: $("#city").val(),
-						state: $("#state").val(),
-						zipcode: $("#zipcode").val(), 
-						description: $("#description").val()
-								 };
-
-								 var response;
-								 $.post(
-										 url,
-										 JSON.stringify(submit),
-										 function(result)
-										 {
-												 r = result.message;
-												 console.log(r);
-											 });
+    var response;
+    
+    $.post(
+            url,
+            JSON.stringify(submit),
+            function(result)
+            {
+                    r = result.message;
+                    console.log(r);
+                });
 
 }
 
