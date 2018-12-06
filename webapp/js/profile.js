b55ce8973bff1626dfdcdd75aa22b61d25063896
Davis Rollman <drollma@hotmail.com>
2018-12-06 04:46:02 -0500

    almost final updates

--- webapp/js/profile.js
+++ webapp/js/profile.js
@@ -9,12 +9,14 @@ $( document ).ready(function() {
 	$("#zipcode").hide(); 
     $("#cancelbutton").hide();
     $("#editbutton").hide();
+    $("#changeIMG").hide();
     console.log(Cookies.get("profile") === Cookies.get("username"));
     
     if (Cookies.get("profile") === Cookies.get("username"))
     {
         //this profile is logged in user. Allow editing.
         $("#editbutton").show();
+        $("#changeIMG").show();
         //console.log(Cookies.get("profile") === Cookies.get("username"));
     }
     
@@ -55,7 +57,8 @@ function loadData(profileToLoad) {
             console.log(result);
             console.log(result.records[0].imgURL);
             
-            $(".hero-image").css("background-image", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + result.records[0].imgURL + ") !important;");
+            //$(".hero-image").css("background-image", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" + result.records[0].imgURL + ") !important;");
+            $(".hero-image").css("background-image", "url(" + result.records[0].imgURL + ")");
             $("#nameField").text(result.records[0].first + " " + result.records[0].last);
             $("#telephoneField").text(result.records[0].phone);
             $("#emailField").text(result.records[0].email);
@@ -127,15 +130,32 @@ function submitData() {
         url = "https://peopleorderourpatties.com/backend/api2/" + Cookies.get("type") + "s/editProfile.php";
     }
     
+    console.log(url);
+    
     var submit = {  username : Cookies.get("username"),
-                        firstname : $("#name").val(),
-                        telephone : $("#telephone").val(),
+                        usernameBefore : Cookies.get("username"),
+                        first : $("#name").val(),
+                        last : " ",
+                        phone : $("#telephone").val(),
                         email : $("#email").val(),
                         city : $("#city").val(),
                         state : $("#state").val(),
-                        zipcode : $("#zipcode").val(), 
+                        zip : $("#zipcode").val(), 
                         description : $("#description").val() };
-
+/*
+                        $ft->usernameBefore = $data->usernameBefore;
+$ft->username = $data->username;
+$ft->truck_name = $data->truck_name;
+$ft->address = $data->address;
+$ft->city = $data->city;
+$ft->state = $data->state;
+$ft->zip = $data->zip;
+$ft->first = $data->first;
+$ft->last = $data->last;
+$ft->email = $data->email;
+$ft->phone = $data->phone;
+$ft->description = $data->description;*/
+                        
     var response;
     
     $.post(
@@ -144,7 +164,7 @@ function submitData() {
         function(result)
         {
             console.log(result);
-            location.reload();
+            //location.reload();
         });
 }
 
